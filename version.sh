83fa2aa96bdf22e2ac440de332282b407c2bb5dd
rajee-gangadaran <rajee.gangadaran@bitwala.com>
2022-09-07 00:13:07 +0530

    added comment and update source code

--- version.sh
+++ version.sh
@@ -1,1 +1,4 @@
-version=v6.0
+############
+# This Version will create the docker images in ECR, after every changes you have to update the version file to push new ECR Image to Deploy on ECS
+############
+version=v6.2
